6e492bba524c1284ec06ae7a86ca221fa3ce1123
Antonio David Ponce Martínez <zolastrodavid@gmail.com>
2018-05-09 10:22:53 +0200

    First commit

---
+++ Circle.js
@@ -0,0 +1,59 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/QHEQuoIKgNE
+
+var textColors = ["#CA9C7F", "#FBBF66", "#FDC168", "#EEAC80", "#E68A53", "#FCC87E", "#C09770", "#F5BF73"];
+var backgroundColors = ["919349", "#E1D270", "#ACAF66", "#A6AA95", "96994A", "#B8C090", "#CED4AD", "#DFD67F"]
+function TextCircle(x, y) {
+  this.x = x;
+  this.y = y;
+  this.r = 5;
+  var index =  int(random(0, textColors.length));
+  this.color = textColors[index];
+  this.growing = true;
+
+  this.grow = function() {
+    if (this.growing && this.r < 15) {
+      this.r += 2;
+    }
+  }
+
+  this.show = function() {
+    stroke(this.color);
+    strokeWeight(2);
+    fill(this.color);
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+
+  this.edges = function() {
+    return (this.x + this.r >= width || this.x - this.r <= 0 || this.y + this.r >= height || this.y - this.r <= 0)
+  }
+}
+
+
+function BackgroundCircle(x, y) {
+  this.x = x;
+  this.y = y;
+  this.r = 5;
+  var index =  int(random(0, backgroundColors.length));
+  this.color = backgroundColors[index];
+  this.growing = true;
+
+  this.grow = function() {
+    if (this.growing && this.r < 15) {
+      this.r += 2;
+    }
+  }
+
+  this.show = function() {
+    stroke(this.color);
+    fill(this.color);
+    strokeWeight(2);
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+
+  this.edges = function() {
+    return (this.x + this.r >= width || this.x - this.r <= 0 || this.y + this.r >= height || this.y - this.r <= 0)
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,148 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/QHEQuoIKgNE
+
+var circles;
+var spots;
+var minRadius = 10;
+var hasFinishedText = false;
+function preload() {
+  img = loadImage("assets/2017.png");
+}
+
+function setup() {
+  createCanvas(img.width, img.height);
+  var density = displayDensity();
+  pixelDensity(1);
+  img.loadPixels();
+  spots = [];
+  circles = [];
+  for (var x = 0; x < img.width; x++) {
+    for (var y = 0; y < img.height; y++) {
+      var index = x + y * img.width;
+      var c = img.pixels[index*4];
+      var b = brightness([c]);
+      if (b > 1) {
+        spots.push(createVector(x, y));
+      }
+    }
+  }
+
+  console.log(img.width);
+  console.log(img.height);
+  console.log("pixels", img.pixels.length);
+  console.log("spots", spots.length);
+  console.log(density)
+}
+
+function draw() {
+  background(255);
+  // frameRate(20)
+
+  var total = 10;
+  var count = 0;
+  var attempts = 0;
+
+  if(!hasFinishedText) {
+    while (count < total) {
+      var newC = newTextCircle();
+      if (newC !== null) {
+        circles.push(newC);
+        count++;
+      }
+      attempts++;
+      if (attempts > 1000) {
+        hasFinishedText = true;
+        break;
+      }
+    }
+  } else {
+    while (count < total) {
+      var newC = newBackgroundCircle();
+      if (newC !== null) {
+        circles.push(newC);
+        count++;
+      }
+      attempts++;
+      if(attempts > 1000) {
+        minRadius = 8;
+      }
+      if (attempts > 2000) {
+        noLoop();
+        console.log("finished");
+        break;
+      }
+    }
+  }
+  
+
+  for (var i = 0; i < circles.length; i++) {
+    var circle = circles[i];
+
+    if (circle.growing) {
+      if (circle.edges()) {
+        circle.growing = false;
+      } else {
+        for (var j = 0; j < circles.length; j++) {
+          var other = circles[j];
+          if (circle !== other) {
+            var d = dist(circle.x, circle.y, other.x, other.y);
+            var distance = circle.r + other.r;
+
+            if (d - minRadius/2 < distance) {
+              circle.growing = false;
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    circle.show();
+    circle.grow();
+  }
+}
+
+function newTextCircle() {
+  var r = int(random(0, spots.length));
+  var spot = spots[r];
+  var x = spot.x;
+  var y = spot.y;
+
+  var valid = true;
+  for (var i = 0; i < circles.length; i++) {
+    var circle = circles[i];
+    var d = dist(x, y, circle.x, circle.y);
+    if (d < circle.r + minRadius) {
+      valid = false;
+      break;
+    }
+  }
+  if (valid) {
+    return new TextCircle(x, y);
+  } else {
+    return null;
+  }
+}
+
+
+function newBackgroundCircle() {
+  var x = int(random(width));
+  var y = int(random(height));
+
+  var valid = true;
+  for (var i = 0; i < circles.length; i++) {
+    var circle = circles[i];
+    var d = dist(x, y, circle.x, circle.y);
+    if (d < circle.r + minRadius) {
+      valid = false;
+      break;
+    }
+  }
+  if (valid) {
+    return new BackgroundCircle(x, y);
+  } else {
+    return null;
+  }
+}
